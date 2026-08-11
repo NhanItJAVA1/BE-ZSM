e610ba7e2127cff4b5fe5440cf730b532dcec450
Huỳnh Trọng Nhân <142094267+NhanItJAVA1@users.noreply.github.com>
2026-08-11 11:36:38 +0700

    rm env

--- obj/Debug/net10.0/BE-ZSM.AssemblyInfo.cs
+++ obj/Debug/net10.0/BE-ZSM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE-ZSM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+160c8ee76268b40d8eb0edb9bf9edd08e4385cb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4d7ef28560d0cec974b6dedfc812760767453d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE-ZSM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BE-ZSM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
